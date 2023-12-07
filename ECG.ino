43ed8e550122a25ada221659bbf2adc804d150aa
philippeminerve <35769720+philippeminerve@users.noreply.github.com>
2023-12-07 21:00:58 +0100

    Create ECG.ino

---
+++ ECG.ino
@@ -0,0 +1,29 @@
+/*
+  Bouton poussoir + LED
+
+  by Philippe MARTIN
+  modified Sep 2020
+
+  This example code is in the public domain
+
+*/
+
+void setup() {
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+
+}
+
+void loop() {
+  Serial.begin(9600);
+  if((digitalRead(10)==1)||(digitalRead(11)==1)){
+      Serial.println("ECG");
+  }
+  else{
+      Serial.println(analogRead(A1));
+  }
+  delay(100);
+  Serial.end();
+  delay(100);
+}
+
